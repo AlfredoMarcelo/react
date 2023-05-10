9e888dbb5fbf77a91f7bd8915a0ca44bf8ffbcab
Alfredo Supanta Alave <alfredo.supanta@corebiz.ag>
2023-05-10 08:34:02 -0400

    feat: se agrega imagen y nombre a carta de presentacion

--- 02-react/src/App.jsx
+++ 02-react/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
 import CartaPresentacion from './CartaPresentacion'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>HOla desde app</h1>
-      <CartaPresentacion/>
+      <CartaPresentacion name="alfredo"/>
     </>
   )
 }
--- 02-react/src/CartaPresentacion.jsx
+++ 02-react/src/CartaPresentacion.jsx
@@ -1,5 +1,14 @@
-const CartaPresentacion = ()=>{
-    return <h1>Hola desde carta</h1>
-}
+import fotoPerfil from "./assets/foto.jpg";
 
-export default CartaPresentacion
+const CartaPresentacion = () => {
+  const nombre = "alfredo";
+
+  return (
+    <>
+      <img src={fotoPerfil} alt="perro" />
+      <h1>Hola me llamo {nombre} y soy dueño de este perro</h1>
+    </>
+  );
+};
+
+export default CartaPresentacion;
