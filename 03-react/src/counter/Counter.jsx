1583293e995c2b6aff6ce841bf211c91dd3b2b33
Alfredo Supanta Alave <alfredo.supanta@corebiz.ag>
2023-05-14 08:45:19 -0400

    feat: se agrega contador

--- 03-react/src/counter/Counter.jsx
+++ 03-react/src/counter/Counter.jsx
@@ -5,11 +5,15 @@ const Counter = () => {
     function handleCount(){
         setCount(count + 1)
     }
+    function handleCountZero(){
+        setCount(0)
+    }
 
     return(
         <div>
             <p>Haz hecho clic {count} veces a este boton</p>
             <button onClick={handleCount}>Incrementar</button>
+            <button onClick={handleCountZero}>Reiniciar</button>
         </div>
     )
 }
